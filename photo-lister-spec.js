f56a753bdfd342d33e23b7c3c3a27aa33c9ebcf3
Vinny <espinalv1@gmail.com>
2017-03-04 18:49:27 -0500

    Initial commit. Completed flickr-fetcher module along with tests.

---
+++ photo-lister-spec.js
@@ -0,0 +1,8 @@
+var expect      = require('chai').expect,
+    PhotoLister = require('./photo-lister');
+
+describe('PhotoLister', function() {
+    it('should exist', function() {
+        expect(PhotoLister).not.to.be.undefined;
+    });
+});
